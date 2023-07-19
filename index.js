a074b0d0ea429f8c1dd0e9085540ccb3742a11d0
myrinja <myrinjau98@gmail.com>
2023-07-19 21:18:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
 function addingEventListener() {
+   let input;
+    input = document.getElementById('input');
+
+    function clickAlert() {
+       alert('I was clicked!');
+      }
+
+     input.addEventListener('click', clickAlert);
 }
